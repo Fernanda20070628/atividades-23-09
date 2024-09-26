a02ba31affd7a6ff027d9348d7ac3e3e1f2fd019
Fernanda Machado <fernandasilvamachado100@gmail.com>
2024-09-25 23:02:10 -0300

    Update pessoas.js

--- pessoas.js
+++ pessoas.js
@@ -1,5 +1,24 @@
+let altura=0
+let peso=0
+let idade=0
+let novaspessoas=0
+let pvelha=0
+let pjovem=0
+var media =0
 do{
-let altura = Number(console.log ('Digite a altura da pessoa:'));
-let peso = Number(console.log ('Digite o peso da pessoa:'));
-if()
-} while(altura!=0)
+  altura=Number(prompt("Qual a altura?"))
+  peso=Number(prompt("qual o peso?"))
+  idade=Number(prompt("qual a idade?"))
+  if(idade>Pvelha){
+    pvelha=peso
+  }
+  else if(idade<Pjovem){
+    pjovem=altura
+  }
+  let npessoas=++
+  media=(media+idade)/npessoas
+  novaspessoas=prompt("Deseja adicionar mais pessoas?")
+}while(Novaspessoas=='sim')
+console.log(`A altura da pessoa mais jovem é ${pjovem}`)
+console.log(`O peso da pessoa mais velha é ${pvelha}`)
+console.log(`A media das idades é ${media}`)
